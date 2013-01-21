9b654a78c503944196aa498267a0ecd62fd95e46
amQel <zoybel7@wp.pl>
2013-01-21 21:57:24 +0100

    Update skrypt1.sh

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-function opcje
-{
-echo "Lista opcji do wyboru"
-echo "1: Pokaż listę opcje"
-echo "2: Pokaż drzewko"
-echo "3: Szyfrowanie pojedynczego pliku tekstowego"
-echo "4: Szyfrowanie wielu plików"
-echo "5: Zobacz zawartość pliku"
-echo "6: Usuń wszystkie pliki utworzone przez ten program"
-echo "7: Odszyfrowanie pojedynczego pliku tekstowego"
-echo "8: Odszyfrowanie wielu plików"
-echo "9: Zakończ "
-}
 function szyfruj
 {
   echo "Podaj nazwę pliku"
 	read nazwapliku
 	if [ -e "$nazwapliku" ]
 	then
+		plik=`basename "$nazwapliku" .od`
+			if [ "$nazwapliku" == "$plik" ]
+			then
+				plik="$nazwapliku"
+			else
+				mv "$nazwapliku" "$plik"
+			fi
 	{
-	cat "$nazwapliku" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
-	} > "$nazwapliku.sd"
-	echo "Zaszyfrowano do $nazwapliku.sd"
+	cat "$plik" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
+	} > "$plik.sd"
+	echo "Zaszyfrowano do $plik.sd"
+	rm "$plik"
 	else 
 	echo "Brak pliku $nazwapliku"
 	fi
@@ -36,11 +31,17 @@ function usun
 {
 	for nplik in *.sd
 		do 
+		if [ -e "$nplik" ]
+		then
 		rm "$nplik"
+		fi
 		done
 	for nplik2 in *.od
 		do 
+		if [ -e "$nplik2" ]
+		then
 		rm "$nplik2"
+		fi
 		done
 }
 function szyfruj_wiele
@@ -52,10 +53,18 @@ function szyfruj_wiele
 		
 		if [ $answer -eq "1" ]
 		then
-		{
-		cat "$nazwyplikow" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
-		} > "$nazwyplikow.sd"
-		
+		plik=`basename "$nazwyplikow" .od`
+			if [ "$nazwyplikow" == "$plik" ]
+			then
+				plik="$nazwyplikow"
+			else
+				mv "$nazwyplikow" "$plik"
+			fi
+	{
+	cat "$plik" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
+	} > "$plik.sd"
+	echo "Zaszyfrowano do $plik.sd"
+	rm "$plik"
 		fi
 		
 		done
@@ -66,50 +75,62 @@ function odszyfruj
 	read nazwapliku
 	if [ -e "$nazwapliku" ]
 	then
+		plik=`basename "$nazwapliku" .sd`
+			if [ "$nazwapliku" == "$plik" ]
+			then
+				plik="$nazwapliku"
+			else
+				mv "$nazwapliku" "$plik"
+			fi
 	{
-	cat "$nazwapliku" | tr 'n-za-mN-ZA-M' 'a-zA-Z' 
-	} > "$nazwapliku.sd"
-	echo "Zaszyfrowano do $nazwapliku.od"
+	cat "$plik" | tr 'n-za-mN-ZA-M' 'a-zA-Z' 
+	} > "$plik.od"
+	echo "Odszyfrowano do $plik.od"
+	rm "$plik"
 	else 
 	echo "Brak pliku $nazwapliku"
 	fi
 }
 function odszyfruj_wiele
 {
-	for nazwyplikow in *
+	for nazwapliku in *.sd
 		do
-		echo "Szyfrować plik $nazwyplikow ?[0/1]" 
+		echo "Odszyfrować plik $nazwapliku ?[0/1]" 
 		read answer
 		
 		if [ $answer -eq "1" ]
 		then
-		{
-		cat "$nazwyplikow" | tr 'n-za-mN-ZA-M' 'a-zA-Z' 
-		} > "$nazwyplikow.od"
-		
+		plik=`basename "$nazwapliku" .sd`
+			if [ "$nazwapliku" == "$plik" ]
+			then
+				plik="$nazwapliku"
+			else
+				mv "$nazwapliku" "$plik"
+			fi
+	{
+	cat "$plik" | tr 'n-za-mN-ZA-M' 'a-zA-Z' 
+	} > "$plik.od"
+	echo "Odszyfrowano do $plik.od"
+	rm "$plik"
 		fi
 		
 		done
 }
-opcje
-d=10;
 
-until [ "$d" -eq 9 ]; do 
-read d
-	case "$d" in
-	
-	"1") opcje ;;
-	"2") tree ;;
-	"3") szyfruj ;;
-	"4") szyfruj_wiele ;;
-	"5") wyswietl ;;
-	"6") usun ;;
-	"7") odszyfruj ;;
-	"8") odszyfruj_wiele ;;	
-	"9") echo "Kończę skrypt..." ;;
+echo "Co chcesz zrobić z plikami?"
+	select x in "Pokaż Drzewko" "Szyfruj" "Szyfruj Wiele" "Wyświetl plik" "Usuń Wszystkie pliki stworzone przez ten program" "Odszyfruj" "Odszyfruj wiele" "Zakończ"
+do
+  case $x in
+	"Pokaż Drzewko") tree ;;
+	"Szyfruj") szyfruj ;;
+	"Szyfruj Wiele") szyfruj_wiele ;;
+	"Wyświetl plik") wyswietl ;;
+	"Usuń Wszystkie pliki stworzone przez ten program") usun ;;
+	"Odszyfruj") odszyfruj ;;
+	"Odszyfruj wiele") odszyfruj_wiele ;;	
+	"Zakończ") echo "Kończę skrypt... Have a nice day."; break ;;
       *) echo "Nic nie wybrałeś" ;;
-    
-	esac
+  esac
+done
 
-done 
 exit 0
